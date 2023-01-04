34729e05e94cb67206e1618fda9b32d338372f5c
tareqzoubii <tareqzoubi4@gmail.com>
2023-01-04 14:18:49 +0200

    V2.0

---
+++ app/components/ReportTable.js
@@ -0,0 +1,52 @@
+import Table from "react-bootstrap/Table";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { timeSlots, sales } from "./data";
+export default function ReportTable({ report }) {
+  let count = 0
+  return (
+    <div>
+      <Table bordered hover >
+        <thead className="bg-green-900 py-2 px-10 rounded-md text-white font-bold hover:bg-green-800">
+          <tr>
+            <th>Location</th>
+            {timeSlots.map((slot) => {
+              return <th>{slot}</th>;
+            })}
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {report.length > 0 &&
+            report.map(({ location }, index) => {
+                let total = 0
+                count = count + 1
+                if (count > 6){
+                    count = 0
+                }
+              return (
+                <tr>
+                  <td className="py-3">{location}</td>
+                  {sales[count].map((hour) => {
+                    total = total + hour
+                    return <td>{hour}</td>;
+                  })}
+                <td>{total} </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
